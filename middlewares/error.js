350cfb6f43b9a366b0f07d5df5d6ff593bfd2cfa
Supreme <supremesuwal9@gmail.com>
2026-07-02 23:21:16 +0545

    custom error handling

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,10 +3,22 @@ const express=require('express')
 const app=express()
 const port=8080
 
-app.use((err,req,res,next)=>{
-    console.log('err')
-    next(err)
+//custom error
+const checkToken=(req,res,next)=>{
+    let {query}=req.query
+    console.log(req.query.query)
+    if(query=='access'){
+        next()
+    }
+    else{
+        throw new Error('access denied')
+    }
+}
+
+app.get('/api',checkToken,(req,res)=>{
+     res.send('hellow')
 })
+
 
 app.listen(port,()=>{
     console.log(`listening to port ${port}`)
