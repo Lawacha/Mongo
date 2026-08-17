077210db49257640ced2c295dfc1f1500bca34b8
Supreme Suwal <supremesuwal9@gmail.com>
2026-08-17 23:34:12 +0545

    Learned Web Cookies

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express=require('express')
 const posts=require('./posts')
 const user=require('./user')
 const app=express()
+const cookieParser=require('cookie-parser')
 
-const port=8080
+const port=3000
+
+app.use(cookieParser('secretcode'));
+
+app.get('/',(req,res)=>{
+   
+    console.log(req.cookies)
+    res.send(`hellow `)
+})
+
+
+app.get('/cookies',(req,res)=>{
+    res.cookie('color',"red",{signed:true});
+    res.send('cookie sent')
+})
+
+app.get('/verify',(req,res)=>{
+    res.send(req.signedCookies)
+})
 
 app.use('/',user)
 app.use('/',posts)
